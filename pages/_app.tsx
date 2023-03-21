9c31cc9599018e9ba72b5bf20adb8b77442aa169
pranam <pranam@gmail.com>
2023-03-21 16:25:57 +0530

    refactor(refactor): refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
-import { SessionProvider } from 'next-auth/react'
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { RecoilRoot } from 'recoil'
 
-function MyApp({ Component, pageProps:{session, ...pageProps} }:AppProps) {
-  return (
 
-  <SessionProvider session={session}>
-    <RecoilRoot>
-   <Component {...pageProps}/>  
-    </RecoilRoot>
-  </SessionProvider>)
-}
+function MyApp({ Component, pageProps}:AppProps) {
+  return <Component {...pageProps}/>  
+  }
 
 export default MyApp
